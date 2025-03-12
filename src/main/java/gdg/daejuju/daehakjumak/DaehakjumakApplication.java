96ef2c9048afb8313507977bc72e97445bc236c2
lwj0831 <wjl0831@gmail.com>
2025-03-12 21:07:27 +0900

    initial setting

---
+++ src/main/java/gdg/daejuju/daehakjumak/DaehakjumakApplication.java
@@ -0,0 +1,13 @@
+package gdg.daejuju.daehakjumak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaehakjumakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaehakjumakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gdg/daejuju/daehakjumak/DaehakjumakApplicationTests.java
@@ -0,0 +1,13 @@
+package gdg.daejuju.daehakjumak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaehakjumakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
